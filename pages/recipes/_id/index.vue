1d507eb6b9201e2cde014a756252aec131f11cb1
genius-softdev <webdevelopercrane@gmail.com>
2020-03-12 12:15:12 +0700

    Implement individual recipe route using id

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -1,32 +1,26 @@
 <template>
-  <section class="recipes">
-    <Recipe
-      v-for="recipe in recipes"
-      :key="recipe.id"
-      :thumbnail="recipe.thumbnail"
-      :title="recipe.title"
-      :id="recipe.id"
-      :previewText="recipe.previewText"
-    />
-  </section>
+    <section class="single-recipe">
+        <h1>{{ recipe.title }}</h1>
+        <div>
+            <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+        </div>
+        <p>{{ recipe.description }}</p>
+    </section>
 </template>
 
 <script>
-import Recipe from "@/components/Recipe";
 export default {
-  components: {
-    Recipe
-  },
-  asyncData() {
+      asyncData(context) {
     //   return axios.get()
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         resolve({
-          recipes: [
+          recipe: [
             {
               id: "1",
               title: "Delicious Pizza",
               previewText: "Delicious Pizza",
+              description: "This is really a delicious meal!", 
               thumbnail:
                 "https://assets.bonappetit.com/photos/597f6564e85ce178131a6475/16:9/w_1200,c_limit/0817-murray-mancini-dried-tomato-pie.jpg"
             },
@@ -34,22 +28,29 @@ export default {
               id: "2",
               title: "See Food Salad",
               previewText: "Very tasty indeed!",
+              description: "This is really a delicious meal!", 
               thumbnail:
                 "https://www.cdc.gov/features/salmonella-food/salmonella-food_456px.jpg"
             }
-          ]
+          ].find(el => el.id === context.params.id)
         });
       }, 1500);
     });
   }
-};
+}
 </script>
 
 <style scoped>
-.recipes {
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: center;
-  align-items: center;
+.single-recipe {
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+}
+
+.recipe-image {
+    width: 100%;
 }
 </style>
